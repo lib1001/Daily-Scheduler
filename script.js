950377f82152b93cf688ef1d247a5b6c426a813d
Lauren <lburke522@gmail.com>
2022-08-25 15:53:24 -0400

    called function to save on refresh

--- script.js
+++ script.js
@@ -1,57 +1,57 @@
-var hour8am = $("#8am");
-var hour9am = $("#9am");
-var hour10am = $("#10am");
-var hour11am = $("#11am");
-var hour12pm = $("#12pm");
-var hour1pm = $("#1pm");
-var hour2pm = $("#2pm");
-var hour3pm = $("#3pm");
-var hour4pm = $("#4pm");
-var hour5pm = $("#5pm");
-var hoursArr = ["hour8am", "hour9am", "hour10am", "hour11am", "hour12pm", "hour1pm", "hour2pm", "hour3pm", "hour4pm", "hour5pm"];
-
-
-
+// sets current date in jumbotron
 var today = moment().format("dddd, MMM Do YYYY");
 $('#currentDay').text(today);
 
-
-// go over this
-$(".saveBtn").on("click", function() {
-    var value = $(this).siblings(".description").val()
-    var time = $(this).parent().attr("id")
-    localStorage.setItem(time, value)
-})
-
-$("#8am .description").text(localStorage.getItem("8am"))
-$("#9am .description").text(localStorage.getItem("9am"))
-$("#10am .description").text(localStorage.getItem("10am"))
-$("#11am .description").text(localStorage.getItem("11am"))
-$("#12pm .description").text(localStorage.getItem("12pm"))
-$("#1pm .description").text(localStorage.getItem("1pm"))
-$("#2pm .description").text(localStorage.getItem("2pm"))
-$("#3pm .description").text(localStorage.getItem("3pm"))
-$("#4pm .description").text(localStorage.getItem("4pm"))
-$("#5pm .description").text(localStorage.getItem("5pm"))
-
+var hour8 = $("#8");
+var hour9 = $("#9");
+var hour10 = $("#10");
+var hour11 = $("#11");
+var hour12 = $("#12");
+var hour1 = $("#1");
+var hour2 = $("#2");
+var hour3 = $("#3");
+var hour4 = $("#4");
+var hour5 = $("#5");
+var hoursArr = ["hour8", "hour9", "hour10", "hour11", "hour12", "hour1", "hour2", "hour3", "hour4", "hour5"];
 
 function changeColor() {
     for (var i = 0; i < hoursArr.length[i]; i++)
-    var currentTime = moment().format("hh")
+    hoursArr[i].removeClass("future past present");
+    var currentTime = moment().hours();
     console.log(currentTime);
     $(".time-block").each(function() {
         console.log($(this));
         var colorTime = parseInt($(this).attr("id"));
         if (currentTime > colorTime) {
-            $(this).children().addClass("past");
+            $(this).addClass("past");
         }
         else if (currentTime === colorTime) {
-            $(this).children().addClass("present");
+            $(this).addClass("present");
         }
         else {
-            $(this).children().addClass("future")
+            $(this).addClass("future")
         }
     })
 }
 
+$(".saveBtn").on("click", function() {
+    var schedule = $(this).siblings(".description").val()
+    var time = $(this).parent().attr("id")
+    localStorage.setItem(time, schedule)
+})
+
+function renderLastRegistered() {
+    $("#8 .description").text(localStorage.getItem("8"))
+    $("#9 .description").text(localStorage.getItem("9"))
+    $("#10 .description").text(localStorage.getItem("10"))
+    $("#11 .description").text(localStorage.getItem("11"))
+    $("#12 .description").text(localStorage.getItem("12"))
+    $("#11 .description").text(localStorage.getItem("1"))
+    $("#12 .description").text(localStorage.getItem("12"))
+    $("#13 .description").text(localStorage.getItem("13"))
+    $("#14 .description").text(localStorage.getItem("14"))
+    $("#15 .description").text(localStorage.getItem("15"))
+    }
+
 changeColor();
+renderLastRegistered();
